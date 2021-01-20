13502b6dd3d77d5a166518920b3335b527e7e88a
RL DEVELOPEMENT <64145740+rldevelopement@users.noreply.github.com>
2021-01-20 19:07:44 +0100

    Add files via upload

---
+++ build.js
@@ -0,0 +1,68 @@
+const builder = require('electron-builder')
+const Platform = builder.Platform
+
+function getCurrentPlatform(){
+    switch(process.platform){
+        case 'win32':
+            return Platform.WINDOWS
+        case 'darwin':
+            return Platform.MAC
+        case 'linux':
+            return Platform.linux
+        default:
+            console.error('Cannot resolve current platform!')
+            return undefined
+    }
+}
+
+builder.build({
+    targets: (process.argv[2] != null && Platform[process.argv[2]] != null ? Platform[process.argv[2]] : getCurrentPlatform()).createTarget(),
+    config: {
+        appId: 'lectronlauncher',
+        productName: 'lectron Launcher',
+        artifactName: '${productName}-setup-${version}.${ext}',
+        copyright: 'Copyright © 2021-2023 Lectron',
+        directories: {
+            buildResources: 'build',
+            output: 'dist'
+        },
+        win: {
+            target: [
+                {
+                    target: 'nsis',
+                    arch: 'x64'
+                }
+            ]
+        },
+        nsis: {
+            oneClick: false,
+            perMachine: false,
+            allowElevation: true,
+            allowToChangeInstallationDirectory: true
+        },
+        mac: {
+            target: 'dmg',
+            category: 'public.app-category.games'
+        },
+        linux: {
+            target: 'AppImage',
+            maintainer: 'RLDevelopement',
+            vendor: 'RLDevelopement',
+            synopsis: 'Launcher of Lectron',
+            description: 'Custom launcher which allows users to join Lectron. All Configurations, and updates are handled automatically.',
+            category: 'Game'
+        },
+        compression: 'maximum',
+        files: [
+            '!{dist,.gitignore,.vscode,docs,dev-app-update.yml,.travis.yml,.nvmrc,.eslintrc.json,build.js}'
+        ],
+        extraResources: [
+            'libraries'
+        ],
+        asar: true
+    }
+}).then(() => {
+    console.log('Build complete!')
+}).catch(err => {
+    console.error('Error during build!', err)
+})
